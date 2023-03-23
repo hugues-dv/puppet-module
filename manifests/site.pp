6721e0b511202ebb3c347136b2e0dd9a52d98f93
hugues-dv <h2vregille@gmail.com>
2023-03-23 15:51:58 +0100

    ajustement puppet

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,24 @@
-if $osfamily == 'debian'{
-  include chrony
+#if $osfamily == 'debian'{
+#  include chrony
+#}
+
+#class { 'motd':
+#  template => 'motd/motd.epp',
+#  content  => "Hello world\n",
+#}
+
+exec { 'my_exec_command':
+  command => 'ls -alrt',
+  path   =>'/usr/bin:/usr/local/bin',
+  cwd  => '/etc/puppetlabs/puppet',
+  logoutput => true,
 }
+
+$result = []
+Exec['my_exec_command']->Notify['results']
+
+notify{'results':
+  message => "c'est la directory puppet : ${result}",
+}
+
+$result = split(trim($::output),"\n")
